c64d2221af163f217cd706cc11def12c6cef20a2
DongHoKim <cha11enge@hanmail.net>
2024-01-24 00:26:09 +0900

    [mod] Pagination 추가

--- src/main/java/com/sns/modeling/application/controller/PostController.java
+++ src/main/java/com/sns/modeling/application/controller/PostController.java
@@ -3,17 +3,15 @@ package com.sns.modeling.application.controller;
 import com.sns.modeling.domain.post.dto.DailyPostCount;
 import com.sns.modeling.domain.post.dto.DailyPostCountRequest;
 import com.sns.modeling.domain.post.dto.PostCommand;
+import com.sns.modeling.domain.post.entity.Post;
 import com.sns.modeling.domain.post.service.PostReadService;
 import com.sns.modeling.domain.post.service.PostWriteService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/posts")
@@ -21,14 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
   final private PostWriteService postWriteService;
   final private PostReadService postReadService;
-
   @PostMapping
   public Long create(PostCommand command){
     return this.postWriteService.create(command);
   }
-
   @GetMapping("/daily-post-count")
   public List<DailyPostCount> getDailyPostCounts(@ModelAttribute DailyPostCountRequest request){
     return this.postReadService.getDailyPostCount(request);
+  }
+  @GetMapping("/member/{memberId}")
+  public Page<Post> getPost(
+          @PathVariable("memberId") Long memberId,
+           Pageable pageable
+  ){
+    return this.postReadService.getPosts(memberId, pageable);
   }
 }
